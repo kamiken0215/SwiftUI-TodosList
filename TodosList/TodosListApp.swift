b43ce41eabacc474190dc129d334e1e3d05a8195
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-16 19:35:06 +0900

    Initial Commit

---
+++ TodosList/TodosListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosListApp.swift
+//  TodosList
+//
+//  Created by 神山賢太郎 on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct TodosListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
